59f44eeaeebf006fe113673780bed00c1f5308ba
Michael <michaelchapa1@gmail.com>
2020-10-01 16:59:07 -0500

    Updates

---
+++ precache-manifest.77fa0f5e3432e2a17ed9e4322506c501.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ff577a81ec05eb8e10d8f3f2ce4d98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "32a5dba5fb154ee663d1",
+    "url": "/portfolio/static/css/main.bd40a7aa.chunk.css"
+  },
+  {
+    "revision": "7639985f3bfc8316af16",
+    "url": "/portfolio/static/js/2.3d832c26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3d832c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a5dba5fb154ee663d1",
+    "url": "/portfolio/static/js/main.1d269021.chunk.js"
+  },
+  {
+    "revision": "fbd91a1454b7cd10428f",
+    "url": "/portfolio/static/js/runtime-main.c34b28cb.js"
+  },
+  {
+    "revision": "0304825991ed25ea2592f84c2d8279c3",
+    "url": "/portfolio/static/media/aqua.03048259.gif"
+  },
+  {
+    "revision": "9c330f210bad7dee2bda0f0fd59ec88e",
+    "url": "/portfolio/static/media/aqua_gold.9c330f21.gif"
+  },
+  {
+    "revision": "073abd06ae8b982db63a0d7e5631f14d",
+    "url": "/portfolio/static/media/manteca_cat_ghost.073abd06.jpg"
+  }
+]);
