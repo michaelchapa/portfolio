ca44aa7753b7501ee699d95c662d7f7181dea54e
Michael <michaelchapa1@gmail.com>
2020-10-10 22:20:53 -0500

    switch comps

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render(){
         return (
-            <header className = 'header'>
-                <h1>Michael Chapa</h1>
+            <header onclick = "" className = 'header'>
+                <h1 onclick = "">Michael Chapa</h1>
             </header>
         );
     }
