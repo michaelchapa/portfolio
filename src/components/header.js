189855616a4afb6e77e1554f5962366a8bc58859
Michael Chapa <michaelchapa1@gmail.com>
2020-10-13 18:50:42 -0500

    Updated responsiveness of Mobile.

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render(){
         return (
-            <header onclick = "" className = 'header'>
-                <h1 onclick = "">Michael Chapa</h1>
+            <header className = 'header'>
+                <h1>Michael Chapa</h1>
             </header>
         );
     }
--- src/components/main.js
+++ src/components/main.js
@@ -3,18 +3,18 @@ import React, { Component } from 'react'
 export default class Main extends Component {
     render(){
         return (
-           <main onclick = "" className = 'main '>
-               <p onclick = "">Engineering a better future by working where my passion lies.
+           <main className = 'main '>
+               <p>Engineering a better future by working where my passion lies.
                    Data, Analytics, Visualization, Distributed Computing, Machine Learning, 
                    these are a few which come to mind. 
                 </p>
-                <p onclick = "">Graduating Fall 2020 from the University of Texas at San Antonio 
+                <p>Graduating Fall 2020 from the University of Texas at San Antonio 
                     with a B.S. in Computer Science.
                 </p>
-                <p onclick = "">Proud Latino and Texan. I don't think I'll 
+                <p>Proud Latino and Texan. I don't think I'll 
                     ever leave this place, the food is too good, the people are
                     even better.</p> 
-                <p onclick = "">Chat me up on the following: Tacos Al Pastor, Techno Music
+                <p>Chat me up on the following: Tacos Al Pastor, Techno Music
                     Production, Jiu Jitsu, Linux Commands, Stand Up Comedy, 
                     Cloud Computing, Ray Bradbury, Cats, Office Supplies. 
                 </p>
