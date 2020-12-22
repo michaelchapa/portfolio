754477f42251ee63644b466685db5fc3e53b60c2
Michael Chapa <michaelchapa1@gmail.com>
2020-12-21 22:36:05 -0600

    Updated the text and some links.

--- src/components/aside.js
+++ src/components/aside.js
@@ -6,7 +6,7 @@ export default class Aside extends Component {
             <aside className = 'menu'>
                 <a id = 'nav1' rel = 'noopener noreferrer' href = 'https://github.com/michaelchapa' target ='blank'>GitHub</a>
                 <a id = 'nav2' rel = 'noopener noreferrer' href = 'https://linkedin.com/in/michaelchapa1' target = '_blank'>LinkedIn</a>
-                <a id = 'nav3' rel = 'noopener noreferrer' href = 'https://soundcloud.com/groove-bodega' target = '_blank'>Groove Bodega</a>
+                <a id = 'nav3' rel = 'noopener noreferrer' href = 'https://michaelchapa.github.io/otherNews' target = '_blank'>Read otherNews</a>
                 <a id = 'nav4' rel = 'noopener noreferrer' href = 'https://soundcloud.com/4d69636861656c' target = '_blank'>4d69636861656c</a>
             </aside>
         );
--- src/components/main.js
+++ src/components/main.js
@@ -5,18 +5,19 @@ export default class Main extends Component {
         return (
            <main className = 'main '>
                <p>Engineering a better future by working where my passion lies.
-                   Data, Analytics, Visualization, Distributed Computing, Machine Learning, 
-                   these are a few which come to mind. 
+                   Data, Analytics, Visualization, Distributed Computing and Machine Learning.
                 </p>
-                <p>Graduating Fall 2020 from the University of Texas at San Antonio 
+                <p>Graduated Fall 2020 from the University of Texas at San Antonio 
                     with a B.S. in Computer Science.
                 </p>
-                <p>Proud Latino and Texan. I don't think I'll 
+                <p>Chat me up about Techno Music Production, 
+                    Jiu Jitsu, Cloud Computing, Stand Up Comedy, 
+                    Finance, Ray Bradbury, Cats, Tacos Al Pastor 
+                    and Office Supplies. 
+                </p>
+                <p>Proud Mexican and Texan. I don't think I'll 
                     ever leave this place, the food is too good, the people are
-                    even better.</p> 
-                <p>Chat me up on the following: Tacos Al Pastor, Techno Music
-                    Production, Jiu Jitsu, Linux Commands, Stand Up Comedy, 
-                    Cloud Computing, Ray Bradbury, Cats, Office Supplies. 
+                    even better.
                 </p>
            </main>
         );
